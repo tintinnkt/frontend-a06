c44e6360952f57c3f36cec572f4dc88567abef42
tintinnkt <t.tintinnkt@gmail.com>
2025-03-13 13:35:05 +0700

    add interactive card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { Rating } from "@mui/material";
+import InteractiveCard from "./InteractiveCard";
 
 interface CardProps {
   venueName: string;
@@ -19,26 +20,28 @@ const Card: React.FC<CardProps> = ({ venueName, imgSrc, onRatingChange }) => {
   };
 
   return (
-    <div className="border rounded-lg pb-4 shadow-md w-64 bg-slate-100">
-      <img
-        src={imgSrc}
-        alt={venueName}
-        className="w-full h-40 object-cover rounded-md"
-      />
-      <div className="px-3">
-        <h3 className="text-lg font-semibold mt-2 text-slate-700">
-          {venueName}
-        </h3>
-
-        <Rating
-          id={`${venueName} Rating`}
-          name={`${venueName} Rating`}
-          data-testid={`${venueName} Rating`}
-          value={rating}
-          onChange={handleRatingChange}
+    <InteractiveCard>
+      <div className="border rounded-lg pb-4 shadow-md w-full bg-slate-100">
+        <img
+          src={imgSrc}
+          alt={venueName}
+          className="w-full h-40 object-cover rounded-md"
         />
+        <div className="px-3">
+          <h3 className="text-lg font-semibold mt-2 text-slate-700">
+            {venueName}
+          </h3>
+
+          <Rating
+            id={`${venueName} Rating`}
+            name={`${venueName} Rating`}
+            data-testid={`${venueName} Rating`}
+            value={rating}
+            onChange={handleRatingChange}
+          />
+        </div>
       </div>
-    </div>
+    </InteractiveCard>
   );
 };
 
